00a523cb55d9840e65310a8f0bcd3e5566922378
plj <335792242@qq.com>
2020-04-18 15:57:06 +0800

    login,register-font

--- font/src/router/index.js
+++ font/src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import login from "../views/login"
+import register from "../views/register"
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // }
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: register
     }
   ]
 })
+
+
+
+
